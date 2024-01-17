d05cc35960904bd8c3953d49b6f79e2d38606c09
ilvimafr <ilvimafr@gmail.com>
2024-01-17 21:34:53 +0200

    feat(logout): logout page

---
+++ pages/logout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['redirect-if-logged-out']
+});
+
+useSupabaseClient().auth.signOut();
+navigateTo('/');
+</script>
+
+<template>
+  Logging out...
+</template>
