d40b3b93db57640ceea1e3eaaeed78f0aaebd7bd
ilvimafr <ilvimafr@gmail.com>
2024-01-17 22:01:27 +0200

    style(header): highlight active header link with pseudo-element

--- components/Header/Link.vue
+++ components/Header/Link.vue
@@ -19,7 +19,10 @@ defineProps({
   <NuxtLink
     :href="href"
     class="transition-colors duration-150 hover:transition-none hover:text-emerald-500"
-    activeClass="font-semibold text-emerald-500 cursor-default"
+    activeClass="
+      relative font-semibold text-emerald-500 cursor-default
+      after:absolute after:w-[10px] after:h-[5px] after:left-[55%] after:-bottom-2 after:-translate-x-1/2 after:bg-emerald-500 after:rounded-lg
+    "
   >
     <UIcon :name="iconName" class="align-[-2px]"/>
     {{ title }}
