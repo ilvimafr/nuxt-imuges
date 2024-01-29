6c1c392bd96641c227bd85945288657bf39da068
ilvimafr <ilvimafr@gmail.com>
2024-01-29 20:37:10 +0200

    feat(components): move components into a spearate component

---
+++ components/Comments.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="flex mt-5 group">
+    <div class="min-w-16 max-w-16 h-16 bg-zinc-700 rounded-full mr-6"></div>
+    <div>
+      <b class="text-lg">ilvimafr</b>
+      <p class="leading-5 text-zinc-700 dark:text-zinc-200">
+        Comment body
+      </p>
+      <div class="flex gap-4 text-zinc-500 dark:text-zinc-500 mt-1">
+        <a href="#" class="hover:text-emerald-500">
+          0
+          <UIcon name="i-heroicons-heart" class="align-[-3px]"/>
+        </a>
+        <div>
+          4 hours ago
+        </div>
+        <a href="#" class="invisible group-hover:visible hover:text-emerald-500">Reply</a>
+        <a href="#" class="invisible group-hover:visible hover:text-emerald-500">Report</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="flex mt-5 group">
+    <div class="min-w-16 max-w-16 h-16 bg-zinc-700 rounded-full mr-6"></div>
+    <div>
+      <b class="text-lg">ilvimafr</b>
+      <p class="leading-5 text-zinc-700 dark:text-zinc-200">
+        Comment body
+      </p>
+      <div class="flex gap-4 text-zinc-500 dark:text-zinc-500 mt-1">
+        <a href="#" class="hover:text-emerald-500">
+          0
+          <UIcon name="i-heroicons-heart" class="align-[-3px]"/>
+        </a>
+        <div>
+          4 hours ago
+        </div>
+        <a href="#" class="invisible group-hover:visible hover:text-emerald-500">Reply</a>
+        <a href="#" class="invisible group-hover:visible hover:text-emerald-500">Report</a>
+      </div>
+    </div>
+  </div>
+
+</template>
